b7b8060947da6b786e4008a0c50aa33d22c8b3b8
koal44 <random.var.ny@gmail.com>
2026-05-09 21:13:40 -0700

    add selector perf harness and class regex cache

--- test_new/global.d.ts
+++ test_new/global.d.ts
@@ -1,4 +1,5 @@
 import type { PwHelpers } from './browser/harness/browser';
+import type { PerfHelpers } from './perf/harness/perf-scenario';
 
 export {};
 
@@ -7,5 +8,7 @@ declare global {
     __pwHelpers: PwHelpers;
     __pwXml: XMLDocument;
     __pwArg: unknown;
+    __perfHelpers: PerfHelpers;
+    __perfXml: XMLDocument;
   }
 }
